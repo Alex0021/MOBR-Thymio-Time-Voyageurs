f609faca3a2d5d5912fc7ab6375794a1e504899f
Quentin <sinnax96@gmail.com>
2023-11-27 15:14:21 +0100

    main

--- Plot/plot.py
+++ Plot/plot.py
@@ -1,3 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import display, clear_output
+import time
+import math
+
 def plot(abs_pos, thymio_coords, goal_position, map_global, closest_angle):
     # Plot
     # Plot Map
@@ -19,12 +25,12 @@ def plot(abs_pos, thymio_coords, goal_position, map_global, closest_angle):
     thymio_X = abs_pos[0][0]+thymio_coords_rotated[:,0]
     thymio_y = abs_pos[0][1]+thymio_coords_rotated[:,1]
     plt.plot(thymio_X, thymio_y)
+    
     # Plot Direction
-    if state==1:
-        x, y, angle = abs_pos[0][:]
-        dx = math.cos(closest_angle)#angle_radians
-        dy = math.sin(closest_angle)#closest_angle
-        plt.quiver(x, y, dx, dy, angles='xy', scale_units='xy', scale=0.1, color='r', width=0.01)
+    x, y, angle = abs_pos[0][:]
+    dx = math.cos(closest_angle)#angle_radians
+    dy = math.sin(closest_angle)#closest_angle
+    plt.quiver(x, y, dx, dy, angles='xy', scale_units='xy', scale=0.1, color='r', width=0.01)
 
 
     plt.show()
