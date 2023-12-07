20c3bec3c49e1c8870390c90100fd019f6ec6719
rfuleiha <117180588+rfuleiha@users.noreply.github.com>
2023-12-07 22:23:54 +0100

    Add files via upload

--- Kalman/Kalman.py
+++ Kalman/Kalman.py
@@ -12,7 +12,7 @@ def calculate_control_matrix(gamma, delta_t):
                
 import numpy as np
 
-def Kalman(z_t_observation_vector, state_0, control_vector_t_minus_1, P_t_minus_1, vision):
+def Kalman(z_t_observation_vector, state_0, control_vector_t_minus_1, P_t_minus_1):
     """
     Kalman filter implementation for state estimation.
 
@@ -21,7 +21,6 @@ def Kalman(z_t_observation_vector, state_0, control_vector_t_minus_1, P_t_minus_
         state_0 (numpy.ndarray): The initial state vector at time t-1 [m_x[m],m_y[m],m_theta[rad]].
         control_vector_t_minus_1 (numpy.ndarray): The control vector at time t-1 [m/s,rad/s].
         P_t_minus_1 (numpy.ndarray): The error covariance matrix at time t-1.
-        vision (bool): Flag indicating whether camera vision is used.
 
     Returns:
         tuple: A tuple containing the new predicted state estimate and the updated error covariance matrix.
@@ -32,13 +31,11 @@ def Kalman(z_t_observation_vector, state_0, control_vector_t_minus_1, P_t_minus_
     # 1st step: prediction state estimate 
     new_predicted_state_t = np.dot(A_t_minus_1, state_0) + np.dot(calculate_control_matrix(state_0[2], delta_t), control_vector_t_minus_1) + sensor_noise
     P_t = np.dot(np.dot(A_t_minus_1, P_t_minus_1), A_t_minus_1.T) + Q_t
-    
-    if vision:
-        # If camera is used, we implement the second step: update state estimate. Otherwise, we only do the first step.
-        measurement_residual_y_t = z_t_observation_vector - (np.dot(H_t, new_predicted_state_t) + sensor_noise)
-        S_t = np.dot(np.dot(H_t, P_t), H_t.T) + R_t
-        K_t = np.dot(np.dot(P_t, H_t.T), np.linalg.pinv(S_t))
-        new_predicted_state_t += np.dot(K_t, measurement_residual_y_t)
-        P_t -= np.dot(np.dot(K_t, H_t), P_t)
+    # The second step: update state estimate
+    measurement_residual_y_t = z_t_observation_vector - (np.dot(H_t, new_predicted_state_t) + sensor_noise)
+    S_t = np.dot(np.dot(H_t, P_t), H_t.T) + R_t
+    K_t = np.dot(np.dot(P_t, H_t.T), np.linalg.pinv(S_t))
+    new_predicted_state_t += np.dot(K_t, measurement_residual_y_t)
+    P_t -= np.dot(np.dot(K_t, H_t), P_t)
 
     return new_predicted_state_t, P_t
